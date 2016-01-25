d9c0fac277b9f6ef6c68d135b93fe87de4d01ea0
Anatol Manoila  <anatol.manoila@tellence.com>
2016-01-25 20:13:17 +0200

    add navigation

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    res.render('index', {title: 'Hello from render', list: ['a', 'b', 'c']});
+    res.render('index', {
+        title: 'Hello ',
+        nav: [{
+            Link:'/books', Text:'Books'
+        },
+        {
+            Link:'/authors', Text:'Authors'
+        }]
+    });
 });
 
 var port = process.env.PORT || 3000;
