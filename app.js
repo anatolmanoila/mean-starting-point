300c74f502422731cd02eba085208b3e015d8e3d
Anatol Manoila  <anatol.manoila@tellence.com>
2016-01-25 19:56:56 +0200

    adding ejs

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+//app.use(express.static('src/views'));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    res.send('Hi Aema');
+    res.render('index', {title: 'Hello from render', list: ['a', 'b', 'c']});
 });
 
 var port = process.env.PORT || 3000;
